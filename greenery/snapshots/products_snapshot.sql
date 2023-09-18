259647dd3195be3380b853212708729252e32550
Ian Young <ian@young.gg>
2023-09-18 10:14:21 +0000

    style: Format correction

--- greenery/snapshots/products_snapshot.sql
+++ greenery/snapshots/products_snapshot.sql
@@ -1,13 +1,13 @@
 {% snapshot products_snapshot %}
 
     {{
-    config(
-        target_database = target.database,
-        target_schema = target.schema,
-        strategy='check',
-        unique_key='product_id',
-        check_cols=['inventory'],
-    )
+        config(
+            target_database = target.database,
+            target_schema = target.schema,
+            strategy='check',
+            unique_key='product_id',
+            check_cols=['inventory'],
+        )
     }}
 
     select * 

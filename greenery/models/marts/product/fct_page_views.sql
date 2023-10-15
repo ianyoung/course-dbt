b64c3f63e399f10f1e4e20f2ec9829796a2a3979
Ian Young <ian@young.gg>
2023-10-15 15:39:20 +0000

    Add macro for event_types

--- greenery/models/marts/product/fct_page_views.sql
+++ greenery/models/marts/product/fct_page_views.sql
@@ -1,3 +1,10 @@
+{% set event_types = [
+    'page_view',
+    'add_to_cart',
+    'checkout',
+    'package_shipped'
+] %}
+
 with 
     session_timing_agg as (
         select *
@@ -10,10 +17,9 @@ select
     coalesce(e.product_id, oi.product_id) as product_id,
     session_started_at,
     session_ended_at,
-    sum(case when e.event_type = 'page_view' then 1 else 0 end) as page_views,
-    sum(case when e.event_type = 'add_to_cart' then 1 else 0 end) as add_to_carts,
-    sum(case when e.event_type = 'checkout' then 1 else 0 end) as checkouts,
-    sum(case when e.event_type = 'package_shipped' then 1 else 0 end) as packages_shipped,
+    {%- for event_type in event_types %}
+        {{ sum_of('e.event_type', event_type ) }} as {{ event_type }}s,
+    {%- endfor %}
     datediff(minute, session_started_at, session_ended_at) as session_length_minutes
 from 
     {{ ref('stg_postgres__events') }} as e
